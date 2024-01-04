33cda79a5d7ed23bf9d7beef2975b72734092fd0
Josh Buker <crypto@joshbuker.com>
2024-01-04 14:15:19 -0800

    Fix flaky case sensitivity for list factory

--- spec/factories/lists.rb
+++ spec/factories/lists.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :list do
     user
 
-    title { Faker::String.random }
+    title { "a#{Faker::String.random}" }
     color { Faker::Color.hex_color }
     icon { ['local_offer'].sample }
     order { 1 }
