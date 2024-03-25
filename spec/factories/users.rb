95c69a952faed4c5f463fecede6aea330cb59cbb
Josh Buker <git-commit@joshbuker.com>
2024-03-25 16:59:21 -0700

    Use unique username/email for User factory
    Avoid accidentally triggering duplicate username validator

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    username    { Faker::Internet.username }
+    username    { Faker::Internet.unique.username }
     given_name  { Faker::Name.first_name }
     family_name { Faker::Name.last_name }
-    email       { Faker::Internet.email }
+    email       { Faker::Internet.unique.email }
     password do
       Faker::Internet.password(
         min_length:         14,
