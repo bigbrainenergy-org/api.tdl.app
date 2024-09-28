91a02cb30ba72ace20fca4aab812d12a4e2b4044
Josh Buker <crypto@joshbuker.com>
2024-09-28 10:30:32 -0700

    Disable experimental seeds by default, too slow

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -6,6 +6,6 @@ load(neo_seeds)
 # puts "Loading seed file from: #{void_xxx_seeds}"
 # load(void_xxx_seeds)
 
-generic_user_seeds = Rails.root.join('db/seeds/experimental_seeds.rb')
-puts "Loading seed file from: #{generic_user_seeds}"
-load(generic_user_seeds)
+# generic_user_seeds = Rails.root.join('db/seeds/experimental_seeds.rb')
+# puts "Loading seed file from: #{generic_user_seeds}"
+# load(generic_user_seeds)
